94c4578afd3ec740524ea57966ff4ae30a73672e
Drey-1 <matosandrey11@gmail.com>
2026-09-18 20:40:05 -0300

    feat: update mutation payload type in useUpdatePlanting hook to include status

--- apps/web/src/hooks/useUpdatePlanting.ts
+++ apps/web/src/hooks/useUpdatePlanting.ts
@@ -2,12 +2,16 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { apiFetch } from "@/lib/apiClient";
 import type { CreatePlantingPayload } from "@/types/CreatePlantingPayload";
 
+type UpdatePlantingPayload = CreatePlantingPayload & {
+	status: "ACTIVE" | "FINISHED";
+};
+
 export const useUpdatePlanting = (plantingId: string) => {
 	const queryClient = useQueryClient();
 
 	const { mutate: updatePlanting, isPending: isUpdatingPlanting } = useMutation(
 		{
-			mutationFn: async (payload: Partial<CreatePlantingPayload>) => {
+			mutationFn: async (payload: Partial<UpdatePlantingPayload>) => {
 				await apiFetch(`/plantings/${plantingId}`, {
 					method: "PATCH",
 					body: JSON.stringify(payload),
