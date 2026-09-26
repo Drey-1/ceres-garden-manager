5018f3a1cd2c6e05e484bc6ca7b8db0fdde6eaa2
Drey-1 <matosandrey11@gmail.com>
2026-09-26 17:16:09 -0300

    refactor: replace input elements with FormInput and SubmitButton components in Login page

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import type React from "react";
 import { useState } from "react";
+import FormInput from "@/components/FormInput";
+import SubmitButton from "@/components/SubmitButton";
 import { apiFetch, setAccessToken } from "@/lib/apiClient";
 
 export default function Login() {
@@ -29,9 +31,15 @@ export default function Login() {
 	};
 
 	return (
-		<form action="" onSubmit={handleSubmit} className="min-w-84 flex flex-col gap-6 bg-white/60 border-white border-2 backdrop-blur-md p-4 rounded-xl">
-			<h2 className="text-2xl text-center text-white font-bold text-border text-shadow-lg">Login</h2>
-			<input
+		<form
+			action=""
+			onSubmit={handleSubmit}
+			className="min-w-84 flex flex-col gap-6 bg-white/60 border-white border-2 backdrop-blur-md p-4 rounded-xl"
+		>
+			<h2 className="text-2xl text-center text-white font-bold text-border text-shadow-lg">
+				Login
+			</h2>
+			<FormInput
 				type="email"
 				placeholder="Email:"
 				value={email}
@@ -39,9 +47,8 @@ export default function Login() {
 				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
 					setEmail(e.target.value);
 				}}
-				className="bg-gray-100 text-lg text-gray-700 p-3 rounded placeholder:text-gray-500 focus:bg-white focus:outline-2 focus:outline-[#3F6E4A] focus:border-0  hover:outline-[#A4CBA9] hover:outline-2"
 			/>
-			<input
+			<FormInput
 				type="password"
 				placeholder="Password:"
 				value={password}
@@ -49,11 +56,13 @@ export default function Login() {
 				onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
 					setPassword(e.target.value);
 				}}
-				className="bg-gray-100 text-lg text-gray-700 p-3 rounded placeholder:text-gray-500 focus:bg-white focus:outline-2 focus:outline-[#3F6E4A] focus:border-0  hover:outline-[#A4CBA9] hover:outline-2"
 			/>
-			<button type="submit" className="p-2 text-gray-800 hover:text-white text-lg font-bold border-2 border-[#6BA37E] hover:bg-[#6BA37E] rounded transition-colors">Submit</button>
+			<SubmitButton>Login</SubmitButton>
 			<p className="text-lg text-gray-600">
-				Don't have an account? <Link href={"/register"} className="text-[#56A2E8] font-semibold">Register</Link>
+				Don't have an account?{" "}
+				<Link href={"/register"} className="text-[#56A2E8] font-semibold">
+					Register
+				</Link>
 			</p>
 
 			{error && (
